e5482f57cae522cc7a0e41b711ce62b29aed0256
abousow123 <abousow798@gmail.com>
2022-08-14 18:28:59 +0200

    Init cofig

---
+++ kafka-exemple/src/main/java/com/kafka/kafkaexemple/KafkaExempleApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.kafkaexemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExempleApplication.class, args);
+	}
+
+}
---
+++ kafka-exemple/src/test/java/com/kafka/kafkaexemple/KafkaExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.kafkaexemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
